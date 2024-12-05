4eee63330d1d939d375c0c9f50ac844e0fd82e57
Shellydahan <shellydahan18@gmail.com>
2024-12-05 23:02:35 +0200

    final3

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -182,12 +182,8 @@ public class ArrCharOps {
      *         return -2 if there is an error with the input.
      */
     public static int compareTo(String str1, String str2) {
-        if (str1 == null || str2 == null) { 
-            return -2; 
-        }
-    
         int minLength = Math.min(str1.length(), str2.length());
-    
+
         for (int i = 0; i < minLength; i++) {
             char c1 = str1.charAt(i);
             char c2 = str2.charAt(i);
